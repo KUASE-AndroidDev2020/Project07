3ae0cc6082acee04ddbebd41c104bb25dfc89d81
2020M071 <2020m071@kuas.ac.jp>
2020-07-22 01:22:38 +0900

    Add date in home

--- app/src/main/java/com/example/project07/homeActivity.java
+++ app/src/main/java/com/example/project07/homeActivity.java
@@ -3,21 +3,31 @@ package com.example.project07;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.ScaleAnimation;
+import android.widget.TextView;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 
 
 public class homeActivity extends AppCompatActivity {
 
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        textView = findViewById(R.id.textView);
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MMM-yyyy hh:mm:ss a");
+        String dateTime = simpleDateFormat.format(calendar.getTime());
+        textView.setText(dateTime);
+
     }
 }
-
-
-
