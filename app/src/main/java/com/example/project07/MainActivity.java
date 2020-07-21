e061592b1ae63cdb3e5c4c96b7f835c681a2d71f
2020M071 <2020m071@kuas.ac.jp>
2020-07-22 00:21:42 +0900

    Button screen transition

--- app/src/main/java/com/example/project07/MainActivity.java
+++ app/src/main/java/com/example/project07/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.project07;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button StartButton = findViewById(R.id.Startbutton);
+        StartButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.Startbutton:
+                Intent toLog = new Intent(this,homeActivity.class);
+                startActivity(toLog);
+                break;
+        }
     }
 }
