32829680e413a8514407dc9f57958c0a3b90eb26
2020M071 <2020m071@kuas.ac.jp>
2020-07-21 21:58:33 +0900

    home creat

---
+++ app/src/main/java/com/example/project07/homeActivity.java
@@ -0,0 +1,23 @@
+package com.example.project07;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+
+public class homeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
+
+
+
